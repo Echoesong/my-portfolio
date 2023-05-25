09af8ff52e467a5029effebc8415133673de51ea
Joseph Felker <jayefelker@gmail.com>
2023-05-25 16:00:29 -0500

    styling... wow I hate styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,27 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
 
-const serviceId = process.env.REACT_APP_YOUR_SERVICE_ID
-const templateId = process.env.REACT_APP_YOUR_TEMPLATE_ID
-const publicKey = process.env.REACT_APP_YOUR_PUBLIC_KEY
-
+const serviceId = process.env.REACT_APP_YOUR_SERVICE_ID;
+const templateId = process.env.REACT_APP_YOUR_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_YOUR_PUBLIC_KEY;
 
 export const Contact = () => {
-  const form = useRef();
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(
+      .send(
         serviceId,
         templateId,
-        form.current,
+        {
+          user_name: userName,
+          user_email: userEmail,
+          message: message,
+        },
         publicKey
       )
       .then(
@@ -27,17 +32,44 @@ export const Contact = () => {
           console.log(error.text);
         }
       );
+
+    // Clear form fields after successful submission
+    setUserName("");
+    setUserEmail("");
+    setMessage("");
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="flex justify-center items-center border border-red-400">
+      <form onSubmit={sendEmail} className="flex flex-col justify-center items-center">
+        <label>Name:</label>
+        <input
+        className="w-64"
+          type="text"
+          name="user_name"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <label>Email:</label>
+        <input
+        className='w-64'
+          type="email"
+          name="user_email"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+        />
+
+        <label>Message:</label>
+        <textarea
+          className="w-96"
+          typename="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Contact } from '../components/Contact'
 
 export default function Home(props){
     return (
-        <div className='bg-sky-500'>
+        <div>
             <p>Hello, welcome to my portfolio! Please find my contact links below.</p>
             <Contact/>
 

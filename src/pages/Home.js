1dfc1259ba03cc14f0735020e4aec8b49cc86ec6
Joseph Felker <jayefelker@gmail.com>
2023-05-25 15:00:21 -0500

    add tailwind

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Contact } from '../components/Contact'
 
 export default function Home(props){
     return (
-        <div>
+        <div className='bg-sky-500'>
             <p>Hello, welcome to my portfolio! Please find my contact links below.</p>
             <Contact/>
 
